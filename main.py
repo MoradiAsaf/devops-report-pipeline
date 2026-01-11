56e51a06e105b9d2624676250b68c6e8c22354c2
MoradiAsaf <163124901+MoradiAsaf@users.noreply.github.com>
2026-01-11 13:51:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -23,8 +23,8 @@ def create_html_report(pdf_files, success=True):
 
     links = ""
     for pdf in pdf_files:
-    filename = os.path.basename(pdf)
-    links += f'<li><a href="pdfreports/{filename}" target="_blank">{filename}</a></li>\n'
+        filename = os.path.basename(pdf)
+        links += f'<li><a href="pdfreports/{filename}" target="_blank">{filename}</a></li>\n'
 
     html_content = f"""
 <!DOCTYPE html>
@@ -491,6 +491,7 @@ create_html_report(pdf_files, success=True)
 
 # ✅ פתיחת התיקייה
 #os.startfile(daily_folder)
+
 
 
 
